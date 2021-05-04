014e85610f8a044bed96d1acee361323539cb1f1
justinvega11 <vega22@purdue.edu>
2021-05-04 17:31:47 -0500

    DAO declaration

--- src/main/java/com/revature/daos/AccountDAO.java
+++ src/main/java/com/revature/daos/AccountDAO.java
@@ -1,9 +1,13 @@
 package com.revature.daos;
 
+import java.util.List;
+
 import com.revature.models.Account;
 
 public interface AccountDAO {
 	public Account findById(int id); // return account object given ID
+	public List<Account> findByUserId(int userId);// returns account by user id
 	public boolean addAccount(Account a); // add account to db
-
+	public Account updateAccount(Account update); // return account object given ID
+	public List<Account> findByAccountStatus(String status); // return accounts with status note: might need ot change to statusid
 }
--- src/main/java/com/revature/daos/UserDAO.java
+++ src/main/java/com/revature/daos/UserDAO.java
@@ -1,5 +1,10 @@
 package com.revature.daos;
 
-public interface UserDAO {
+import com.revature.models.User;
 
+public interface UserDAO {
+	public User findById(int id); // return user object by Id
+	public User findByUsername(String name); // return user object by username
+	public boolean updateUser(User update); // replaces user 
+	
 }
